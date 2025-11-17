9f8ba6932123d1ab772cd372e79be9dc80ebfc03
emieldecraemer <emiel.decraemer@student.tsaam.be>
2025-11-17 12:14:37 +0100

    laatste commit

--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,8 @@
 const express = require("express")
+const crlitems = require("../controller/items")
 
 const routes = express.Router()
 
+routes.get("/items", crlitems.login)
 
 module.exports = routes
